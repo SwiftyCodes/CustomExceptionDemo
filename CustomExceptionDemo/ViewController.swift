891596dfee2bceff0bb531c3e9b37914cccb698b
Anurag Kashyap <anurag@anurags-macbook-pro.local>
2019-05-26 14:20:19 +0530

    Initial Commit

---
+++ CustomExceptionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomExceptionDemo
+//
+//  Created by Anurag Kashyap on 26/05/19.
+//  Copyright © 2019 Anurag Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
